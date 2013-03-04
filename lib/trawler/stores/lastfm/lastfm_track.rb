708a7464d61ed25ab2d3008ffd5b36257fe2969f
Mike Gardiner <conversationing@gmail.com>
2013-03-04 13:48:11 +0800

    Sketching out the lastfm_store.

---
+++ lib/trawler/stores/lastfm/lastfm_track.rb
@@ -0,0 +1,15 @@
+module Trawler
+  module Stores
+    class LastfmTrack
+      # name
+      # artist.content => artist_name
+      # album.content => album_name
+      # url
+      # image.small => small_image
+      # image.medium => medium_image
+      # image.large => large_image
+      # date.uts => played_timestamp
+      # date.content => played_datetime
+    end
+  end
+end
---
+++ spec/lib/trawler/stores/lastfm/lastfm_store_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe Trawler::Stores::LastfmStore do
+end
