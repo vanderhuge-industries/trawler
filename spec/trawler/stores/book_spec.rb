4193a269ff44916fdf47aa36d52a47fc2a220b72
Ray Grasso <ray.grasso@gmail.com>
2013-09-25 16:23:32 +0800

    Move specs up a level

--- spec/trawler/stores/book_spec.rb
+++ spec/trawler/stores/book_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Trawler::Stores::Book do
   it { should have_fields(:title, :author) }
   it { should have_field(:last_update) }
+  it { should have_field(:source) }
 
   describe 'highlights' do
     it { should have_many(:highlights) }
