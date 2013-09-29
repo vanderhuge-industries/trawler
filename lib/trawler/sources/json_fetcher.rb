cf7be32d024af9fd243ec148f7ef9ce2ccf36f81
Ray Grasso <ray.grasso@gmail.com>
2013-09-29 19:56:58 +0800

    Add default error handler to fetch

--- lib/trawler/sources/json_fetcher.rb
+++ lib/trawler/sources/json_fetcher.rb
@@ -9,11 +9,13 @@ module Trawler
       class << self
         extend Memoist
 
-        def get(url)
+        def get(url, &error_block)
+          error_block ||= -> r { raise "Fetch of #{url} failed. #{error_response}" }
+
           puts "Fetching #{url} ..."
           response = RestClient.get(url, { accepts: :json })
 
-          yield(response) unless response.code == 200
+          error_block.call(response) unless response.code == 200
 
           JSON.parse(response.body)
         end
