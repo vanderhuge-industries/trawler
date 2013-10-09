cc7af36945f6cb1ef0e71f42055f71a5766ee544
Ray Grasso <ray.grasso@gmail.com>
2013-10-09 16:34:46 +0800

    Clean credentials in logged URL message

--- lib/trawler/sources/json_fetcher.rb
+++ lib/trawler/sources/json_fetcher.rb
@@ -12,7 +12,7 @@ module Trawler
         def get(url, &error_block)
           error_block ||= -> r { raise "Fetch of #{url} failed. #{error_response}" }
 
-          puts "Fetching #{url} ..."
+          puts "Fetching #{cleaned_url url} ..."
           response = RestClient.get(url, { accepts: :json })
 
           error_block.call(response) unless response.code == 200
@@ -21,6 +21,10 @@ module Trawler
         end
 
         memoize :get
+
+        def cleaned_url(url)
+          url.gsub /(https?:\/\/[^:]*:?)[^@]*@?(\S*)/, '\1\2'
+        end
       end
     end
   end
