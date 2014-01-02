e2072cc28ee13556652001c820c5971766c1c832
Ray Grasso <ray.grasso@gmail.com>
2014-01-03 00:30:29 +0800

    Make Trawler play nice with Kaminari

--- lib/trawler.rb
+++ lib/trawler.rb
@@ -2,4 +2,11 @@ require 'mongoid'
 
 require 'trawler/stores/visible'
 
+# Ensure Kaminari is initialized (if it is present) before our models are loaded
+begin; require 'kaminari'; rescue LoadError; end
+if defined? ::Kaminari
+  puts "Kicking off Kaminari init"
+  ::Kaminari::Hooks.init
+end
+
 Dir["#{File.dirname(__FILE__)}/trawler/**/*.rb"].each { |rb| require rb }
