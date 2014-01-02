bdd1eddc063ceb509e7c98b63773cbf93db63071
Ray Grasso <ray.grasso@gmail.com>
2014-01-03 00:32:22 +0800

    Remove debug trace

--- lib/trawler.rb
+++ lib/trawler.rb
@@ -5,7 +5,6 @@ require 'trawler/stores/visible'
 # Ensure Kaminari is initialized (if it is present) before our models are loaded
 begin; require 'kaminari'; rescue LoadError; end
 if defined? ::Kaminari
-  puts "Kicking off Kaminari init"
   ::Kaminari::Hooks.init
 end
 
